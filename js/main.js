83a6014f8675d6eb3dab4b8fa55a03b4aa1d544e
adburke <adburke@fullsail.edu>
2012-09-12 02:04:10 -0500

    Work

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ window.addEventListener("DOMContentLoaded", function(){
 	};
 	
 	// Create select field element and populate with options
-	function formLists(selector, array){
+	function formLists(selector, array, id){
 		var formtTag = document.getElementsByTagName("form"),
 			selectLi = $(selector),
 			makeSelect = document.createElement("select");
-			makeSelect.setAttribute("id", "groups");
+			makeSelect.setAttribute("id", id);
 		for (var i=0, j=array.length; i<j; i++){
 			var makeOption = document.createElement("option");
 			var opText = array[i];
@@ -64,7 +64,7 @@ window.addEventListener("DOMContentLoaded", function(){
 			jobFormData.oDate		= ["Order Date", $("orderdate").value];
 			jobFormData.needDate	= ["Need Date", $("needbydate").value];
 			jobFormData.rushOrder	= ["Rush Order", rushValue];
-			jobFormData.jobType		= ["Job Type", $("groups").value];
+			jobFormData.jobType		= ["Job Type", $("jobTypeList").value];
 			jobFormData.customInfo	= ["Custom Info", $("custom").value];
 			jobFormData.quantity	= ["Quantity", $("qty").value];
 			jobFormData.prodHours	= ["Production Hours", $("production").value];
@@ -112,7 +112,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		};
 	};
 
-	// Make Item Links 
+	// Make Item Links
 	// Create the edit and delete links for each object list displayed
 	function makeItemLinks(key, linksLi){
 		var editLink = document.createElement("a");
@@ -140,7 +140,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		// Grab the data from our item from local storage
 		var value = localStorage.getItem(this.key);
 		var jobFormData = JSON.parse(value);
-
+		console.log(value);
 		// Show the form
 		toggleControl("off");
 
@@ -163,11 +163,21 @@ window.addEventListener("DOMContentLoaded", function(){
 				radios[i].setAttribute("checked", "checked");
 			};
 		};
-		$("groups").value = jobFormData.jobType[1];
+		$("jobTypeList").value = jobFormData.jobType[1];
 		$("custom").value = jobFormData.customInfo[1];
 		$("qty").value = jobFormData.quantity[1];
 		$("production").value = jobFormData.prodHours[1];
 		$("design").value = jobFormData.designEff[1];
+
+		// Remove initial listener from submit button
+		save.removeEventListener("click", validation);
+		// Change submit button to edit button
+		$("submit").value = "Edit Job";
+		var editSubmit = $("submit");
+		// Passes the key value along to the editSubmit event
+		// This allows us to save over the data we are editing instead of creating a new object
+		editSubmit.key = this.key;
+		editSubmit.addEventListener("click", validation);
 
 	};
 
@@ -205,26 +215,133 @@ window.addEventListener("DOMContentLoaded", function(){
 		};
 	};
 	// Validate that all required form fields at least have a value *no error checking yet*
-	function validation(){
-		var elements = document.getElementsByTagName("input");
-		console.log(elements);
-		for (var i =0, j = elements.length; i < j; i++){
-			if (elements[i].required && elements[i].value === ""){
-				console.log("Required Missing");
-				console.log(elements[i]);
-				alert("Required Fields Missing");
-				return;
-			};
-		};
-		saveData();
+	function validation(e){
+		console.log("1 got here");
+		var getCompany = $("company");
+		var getAddress = $("address");
+		var getCity = $("city");
+		var getState = $("state");
+		var getZip = $("zipcode");
+		var getPhone = $("phone");
+		var getEmail = $("email");
+		var getOrderDate = $("orderdate");
+		var getNeedByDate = $("needbydate");
+		var getJobTypeList = $("jobTypeList");
+		var getCustom = $("custom");
+		var getQty = $("qty");
+		var getProduction = $("production");
+
+		// Reset Error Messages
+		errorMsg.innerHTML = "";
+
+		// Get Error Messages
+		var messageAry = [];
+
+		// Validations
+		if (getCompany.value === ""){
+			var companyError = "Please enter a company name.";
+			getCompany.style.border = "1px solid red";
+			messageAry.push(companyError);
+		}
+		if (getAddress.value === ""){
+			var addressError = "Please enter an address.";
+			getAddress.style.border = "1px solid red";
+			messageAry.push(addressError);
+		}
+		if (getCity.value === ""){
+			var cityError = "Please enter a city.";
+			getCity.style.border = "1px solid red";
+			messageAry.push(cityError);
+		}
+		if (getState.value === ""){
+			var stateError = "Please enter a state.";
+			getState.style.border = "1px solid red";
+			messageAry.push(stateError);
+		}
+		if (getZip.value === ""){
+			var zipError = "Please enter a zip code.";
+			getZip.style.border = "1px solid red";
+			messageAry.push(zipError);
+		}
+		var re = /\d{3}-\d{3}-\d{4}/;
+		if(!(re.test(getPhone.value))){
+			var phoneError = "Please enter a valid phone number.";
+			getPhone.style.border = "1px solid red";
+			messageAry.push(phoneError);
+		}
+		var re = /^\w+@[\w.\-]+\.[A-Za-z]{2,3}$/;
+		if(!(re.test(getEmail.value))){
+			var emailError = "Please enter a valid email address.";
+			getEmail.style.border = "1px solid red";
+			messageAry.push(emailError);
+		}
+		var d = new Date();
+		if (getOrderDate.value === ""){
+			var orderDateError = "Please enter a valid order date yyyy-mm-dd";
+			getOrderDate.style.border = "1px solid red";
+			messageAry.push(orderDateError);
+		} else if(Date.parse(getOrderDate.value) > Date.parse(getNeedByDate.value)) {
+			orderDateError = "Invalid - Order date can not fall after due date.";
+			getOrderDate.style.border = "1px solid red";
+			messageAry.push(orderDateError);
+		}
+		if (getNeedByDate.value === ""){
+			var needByDateError = "Please enter a valid due date yyyy-mm-dd";
+			getNeedByDate.style.border = "1px solid red";
+			messageAry.push(needByDateError);
+		} else if(Date.parse(getNeedByDate.value) < Date.parse(getOrderDate.value)) {
+			needByDateError = "Invalid - Due date can not fall before order date.";
+			getNeedByDate.style.border = "1px solid red";
+			messageAry.push(needByDateError);
+		}
+		if(getJobTypeList.value === "--Job Types--"){
+			var JobTypeListError = "Please choose a job type.";
+			getJobTypeList.style.border = "1px solid red";
+			messageAry.push(JobTypeListError);
+		}
+		if(getJobTypeList.value === "Custom" && getCustom.value === ""){
+			var customError = "Please describe the custom job.";
+			getCustom.style.border = "1px solid red";
+			messageAry.push(customError);
+		}
+		if (getQty.value === ""){
+			var qtyError = "Please enter a quantity.";
+			getQty.style.border = "1px solid red";
+			messageAry.push(qtyError);
+		} else if(getQty.value/1 !== getQty.value){
+			var qtyError = "Please enter a valid quantity number.";
+			getQty.style.border = "1px solid red";
+			messageAry.push(qtyError);
+		}
+		if (getProduction.value === ""){
+			var productionError = "Please enter a LOE amount.";
+			getProduction.style.border = "1px solid red";
+			messageAry.push(productionError);
+		} else if(getProduction.value/1 !== getProduction.value){
+			var productionError = "Please enter a valid LOE number.";
+			getProduction.style.border = "1px solid red";
+			messageAry.push(productionError);
+		}
+		if(messageAry.length >= 1){
+			for(var i=0, j = messageAry.length; i<j; i++){
+				var txt = document.createElement("li");
+				txt.innerHTML = messageAry[i];
+				errorMsg.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;
+		} else {
+			storeData();
+		}
 	};
 
 	// Variable defaults
 	var jobTypes = ["--Job Types--", "Banner", "Decal", "Sign", "Custom"];
 	var jobNumCount;
 	var rushValue;
+	var errorMsg = $("errors");
 	// Calls the function to create the select box and populates with job types
-	formLists("jobTypes", jobTypes);
+	formLists("jobTypes", jobTypes, "jobTypeList");
 	// Function called to set or check Job # read only field value
 	jobCount();
 	// Set Link $ Submit Click Events
