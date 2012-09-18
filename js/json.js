74e68739c931380588501fe3d01ca456480fd505
adburke <adburke@fullsail.edu>
2012-09-17 21:01:38 -0500

    Work

---
+++ js/json.js
@@ -0,0 +1,38 @@
+var json = {
+	"5000": {	
+		"jobNum":["Job Num","5000"],
+		"company":["Company","23design"],
+		"address":["Address","123 Test Lane"],
+		"city":["City","Birmingham"],
+		"state":["State","Alabama"],
+		"zipcode":["Zipcode","35040"],
+		"phone":["Phone","555-555-5555"],
+		"email":["Email","orders@23design.biz"],
+		"oDate":["Order Date","2012-12-12"],
+		"needDate":["Need Date","2012-12-13"],
+		"rushOrder":["Rush Order","Yes"],
+		"jobType":["Job Type","Banner"],
+		"customInfo":["Custom Info",""],
+		"quantity":["Quantity","3"],
+		"prodHours":["Production Hours","3"],
+		"designEff":["Design Effort","3"]
+	},
+	"5001": {	
+		"jobNum":["Job Num","5001"],
+		"company":["Company","Layer8"],
+		"address":["Address","675 Try Road"],
+		"city":["City","Huntsville"],
+		"state":["State","Alabama"],
+		"zipcode":["Zipcode","35805"],
+		"phone":["Phone","666-666-6666"],
+		"email":["Email","test@layer8tech.com"],
+		"oDate":["Order Date","2012-12-1"],
+		"needDate":["Need Date","2012-12-5"],
+		"rushOrder":["Rush Order","No"],
+		"jobType":["Job Type","Custom"],
+		"customInfo":["Custom Info","Testing custom box."],
+		"quantity":["Quantity","6"],
+		"prodHours":["Production Hours","4"],
+		"designEff":["Design Effort","2"]
+	}
+}
